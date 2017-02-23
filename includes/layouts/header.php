6b3fa2738a8a56206c2b57a3922330603df0eebd
Keer Liu <Keer Liu>
2017-02-22 23:25:29 -0500

    version 0.1.6

---
+++ includes/layouts/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head>
+		<title>Emily's Piano</title>
+		<link href="CSS/public.css?v=6" media="all" rel="stylesheet" type="text/css" /> <!--?v=5 for not cache -->
+	</head>
+	<body>
+    <div id="header">
+      <h1>Emily's Piano</h1>
+    </div>
