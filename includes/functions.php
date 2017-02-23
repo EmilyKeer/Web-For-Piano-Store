ab434910e63364b4e0b40ded0ffb9387edad5f68
Keer Liu <Keer Liu>
2017-02-23 09:22:20 -0500

    select subject/page show difference without refatoring

--- includes/functions.php
+++ includes/functions.php
@@ -10,7 +10,7 @@
 		global $connection; //already difine $connection at the top
 		$query  = "SELECT * ";
 		$query .= "FROM subjects ";
-		$query .= "WHERE visible = 1 ";
+		//$query .= "WHERE visible = 1 ";
 		$query .= "ORDER BY position ASC";
 		$subject_set = mysqli_query($connection, $query);
 		confirm_query($subject_set);
@@ -19,14 +19,53 @@
 
 	function find_pages_for_subject($subject_id){
 		global $connection;
+		$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
 		$query  = "SELECT * ";
 		$query .= "FROM pages ";
 		$query .= "WHERE visible = 1 ";
-		$query .= "AND subject_id = {$subject_id} ";//space at the end
+		$query .= "AND subject_id = {$safe_subject_id} ";//space at the end
 		$query .= "ORDER BY position ASC";
 		$page_set = mysqli_query($connection, $query);
 		confirm_query($page_set);
 		return $page_set;
+	}
+
+	function find_subject_by_id($subject_id){
+		global $connection;
+		$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);//avoid sql injection;slash special chars
+		$query  = "SELECT * ";
+		$query .= "FROM subjects ";
+		$query .= "WHERE id = {$safe_subject_id} ";
+		$query .= "LIMIT 1";
+		$subject_set = mysqli_query($connection, $query);
+		confirm_query($subject_set);
+		if ($subject = mysqli_fetch_assoc($subject_set)) //to return assoc array
+		{
+			return $subject;
+		}
+		else{
+			return null;
+		}
+
+	}
+
+	function find_page_by_id($page_id){
+		global $connection;
+		$safe_page_id = mysqli_real_escape_string($connection, $page_id);//avoid sql injection;slash special chars
+		$query  = "SELECT * ";
+		$query .= "FROM pages ";
+		$query .= "WHERE id = {$safe_page_id} ";
+		$query .= "LIMIT 1";
+		$page_set = mysqli_query($connection, $query);
+		confirm_query($page_set);
+		if ($page = mysqli_fetch_assoc($page_set)) //to return assoc array
+		{
+			return $page;
+		}
+		else{
+			return null;
+		}
+
 	}
 
 	function navigation($subject_id, $page_id){
@@ -68,5 +107,7 @@
 
 		return $output;
 	}
+
+
 
 ?>
